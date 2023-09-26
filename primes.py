9646aeb0d01ced221040119cd7d1dafdaeb3bb77
Harun Abukar <harun.abukar@kcl.ac.uk>
2023-09-27 00:34:43 +0100

    solution completed and passes all 5 tests

--- primes.py
+++ primes.py
@@ -3,4 +3,20 @@
 
 def primes(number_of_primes):
     list = []
+    first = 2
+    if number_of_primes  < 1:
+        raise ValueError("The value you entered is invalid")
+
+    while len(list) < number_of_primes:
+        
+        prime = True
+        for i in range(len(list)):
+            
+            if first % list[i] == 0:
+                prime = False
+                break
+
+        if prime:
+            list.append(first)
+        first += 1
     return list
